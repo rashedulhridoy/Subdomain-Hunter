5ba125f31ee4ff358585fdd28d91216bd894817b
RH <150352851+rashedulhridoy@users.noreply.github.com>
2024-03-22 11:54:18 +0600

    Update sdh.py

--- sdh.py
+++ sdh.py
@@ -1,7 +1,7 @@
 import requests
 import pyfiglet
 
-
+total_sub = 0
 
 space =" "
 print(space)
@@ -11,24 +11,23 @@ print(ascii_banner)
 x = "Made By Rashedul Hridoy"
 print(x)
 print(space)
-c = "Special Credit: Syed Sakib Alam Mubin and " + "Nabil Rahman"
+c = "Special Credit: Syed Sakib Alam Mubin " + "Nabil Rahman"
 print(c)
 print(space)
 
 
 domain = input("Enter Domain: ")
-total_sub = 0
 
 
 print(space)
 
 print("-----Scanning Started-----")
-print("> If You Find All Subdomains Then Tools With Automatic Stopped, " + "Be Patient")
+print("> If You Find All Subdomains Then Tools With Automatic Stopped, Be Patient")
 
 print(space)
 
 
-with open("Subdomain.txt", "r") as sub:
+with open("D:/py/test.txt", "r") as sub:
     subdomains = sub.read().splitlines()
 
     for subdomain in subdomains:
@@ -37,13 +36,12 @@ with open("Subdomain.txt", "r") as sub:
             response = requests.get(url)
 
             if response.status_code == 200:
-                print(url)
+                print(f"\033[1;32m[+] FOUND : {url}")
                 
                 total_sub += 1
 
         except Exception as e: 
-                pass
+                print(f"\033[1;31m[!] NOT FOUND : {url}")
 
-      
-
-print(f"Scan Finished\n\033[1;32m Total Sub-Domain found: {total_sub}")
+print("Scan Finished")
+print(f"Total Sub-Domain found: {total_sub}")
